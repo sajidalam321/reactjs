426b7e054072102e556f49c57ae75ac5da0e07a4
sajidalam321 <alamraza812@gmail.com>
2021-09-15 19:21:11 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import react from "react";
+import "./style.css";
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Hello World!</h1>
+      <button className="button">Read More</button>
+    </div>
+  );
+};
+
+export default App;
